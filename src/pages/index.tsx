613b215547ccff144121fbb766034eb08c449dcd
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2023-02-26 17:15:49 +0900

    Add workflow

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,17 @@
-import Head from 'next/head'
+import Head from "next/head";
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Create Next App</title>
+        <title>ConvNet Shape Calculator</title>
         <meta name="description" content="ConvNet Shape Calculator" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <h1>
-          ConvNet Shape Calculator
-        </h1>
+        <h1>ConvNet Shape Calculator</h1>
       </main>
     </>
-  )
+  );
 }
