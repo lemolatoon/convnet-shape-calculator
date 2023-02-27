f97606b87ce3620c85231581a67507bc2abdc70c
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2023-02-28 00:17:08 +0900

    Make sure to allow `""` in `useObjectState.ts`

--- src/hooks/useObjectState.ts
+++ src/hooks/useObjectState.ts
@@ -1,14 +1,14 @@
 import { useState } from "react";
 
-type ParamObject = Record<string, number>;
+type ParamObject = Record<string, number | "">;
 
 export const useObjectState = <Keys extends keyof ParamObject>(
-  init: Record<Keys, number>
+  init: Record<Keys, number | ""> 
 ) => {
   const [obj, setObj] = useState(init);
   const dispatch = (key: Keys, val: string) => {
-    const copiedObj: Record<Keys, number> = JSON.parse(JSON.stringify(obj));
-    copiedObj[key] = Number(val);
+    const copiedObj: Record<Keys, number | ""> = JSON.parse(JSON.stringify(obj));
+    copiedObj[key] = val === "" ? "" as const : Number(val);
     setObj(copiedObj);
   };
   return { obj, dispatch };
