78b682a5fd036b5ea15b1a3e17c6481c92b30ab5
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2023-02-28 23:10:30 +0900

    Rewrite Sequential layer implementation

--- src/components/Sequential.tsx
+++ src/components/Sequential.tsx
@@ -1,22 +1,34 @@
 import { LayerComponent } from "@/type/layer";
 import { Size, Tensor } from "@/type/size";
+import { useState } from "react";
 
 export const Sequential =
-  <T extends Size>(layers: LayerComponent<T>[]) =>
+  <T extends Size>(_layers: LayerComponent<T>[]): LayerComponent<T> =>
   (tensor?: Tensor<T>) => {
-    return layers.reduce(
-      (prev, layer) => {
-        const { layer: nextLayer, tensor: nextTensor } = layer(prev.tensor);
+    const [layers, setLayers] = useState(
+      _layers.map((layer, idx) => ({ layer, id: idx }))
+    );
+    const { layerUIs, tensors } = layers.reduce<{
+      layerUIs: { layerUI: React.ReactElement; id: number }[];
+      tensors: (Tensor<T> | undefined)[];
+    }>(
+      ({ layerUIs, tensors }, { layer, id }) => {
+        const { layer: layerUI, tensor: nextTensor } = layer(
+          tensors.slice(-1)[0]
+        );
         return {
-          layer: (
-            <>
-              {prev.layer}
-              {nextLayer}
-            </>
-          ),
-          tensor: nextTensor,
+          layerUIs: [...layerUIs, { layerUI, id }],
+          tensors: [...tensors, nextTensor],
         };
       },
-      { layer: <></>, tensor }
+      { layerUIs: [], tensors: [tensor] }
     );
+    const layer = (
+      <div>
+        {layerUIs.map(({ layerUI, id }) => {
+          return <div key={id}>{layerUI}</div>;
+        })}
+      </div>
+    );
+    return { layer, tensor: tensors.slice(-1)[0] };
   };
