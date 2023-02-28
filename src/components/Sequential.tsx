de936d3ca992c9c6b56b2a8ba07a2c723a532194
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2023-03-01 00:12:18 +0900

    Add drag icon in Sequential

--- src/components/Sequential.tsx
+++ src/components/Sequential.tsx
@@ -1,6 +1,23 @@
 import { LayerComponent } from "@/type/layer";
 import { Size, Tensor } from "@/type/size";
 import { useState } from "react";
+import { MdDragIndicator } from "react-icons/md";
+import styled from "styled-components";
+
+const SequentialGrid = styled.div`
+  display: grid;
+  grid-template-columns: 2rem 1fr;
+`;
+
+const IconWrappingBox = styled.div`
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+`;
+const StyledDragIndicator = styled(MdDragIndicator)`
+  justify-content: center;
+  font-size: 2rem;
+`;
 
 export const Sequential =
   <T extends Size>(_layers: LayerComponent<T>[]): LayerComponent<T> =>
@@ -26,7 +43,14 @@ export const Sequential =
     const layer = (
       <div>
         {layerUIs.map(({ layerUI, id }) => {
-          return <div key={id}>{layerUI}</div>;
+          return (
+            <SequentialGrid key={id}>
+              <IconWrappingBox>
+                <StyledDragIndicator />
+              </IconWrappingBox>
+              <div>{layerUI}</div>
+            </SequentialGrid>
+          );
         })}
       </div>
     );
